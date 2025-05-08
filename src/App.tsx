16596b306c0b7285a0542305e89d2dcf2be112a5
Gabriel Nog <130473061+Bielnog@users.noreply.github.com>
2025-05-08 19:23:22 -0300

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import ModalSignUp from './components/ModalSignUp'
+
+function App() {
+
+  return (
+    <div>
+      <ModalSignUp />
+    </div>
+  )
+}
+
+export default App
